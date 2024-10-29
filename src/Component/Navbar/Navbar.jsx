0a2fba2a7fe915f520452584fa769aa5519eba40
wacho91 <criskol.71@gmail.com>
2024-10-29 11:23:21 -0500

    Arquitecture component Navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,8 +1,61 @@
 
+import { FaCartShopping } from "react-icons/fa6";
+import Logo from "../../assets/food-logo.png";
+
+const Menu = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "Services",
+      link: "/#services",
+    },
+    {
+      id: 3,
+      name: "About",
+      link: "/#about",
+    },
+];
+
 const Navbar = () => {
   return (
     <div>
-        Navbar Component
+        <div>
+            <div>
+                <div>
+                    <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+                        <img 
+                            src={Logo} 
+                            alt="Logo" 
+                            className="w-10"    
+                        />
+                        Foodie
+                    </a>
+                </div>
+                <div>
+                    {/*DarkMode*/}
+                    <div>
+
+                    </div>
+                    <ul>
+                        {Menu.map((menu) => (
+                            <li key={menu.id}>
+                                <a href={menu.link}>
+                                    {menu.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <button className="">
+                        Order 
+                        <FaCartShopping />
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
