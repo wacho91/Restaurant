3fbde611247beb2d8e53b8c6482a4eff40654392
wacho91 <criskol.71@gmail.com>
2024-10-30 10:56:29 -0500

    Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
 import Navbar from "./Component/Navbar/Navbar"
+import Hero from "./Component/Hero/Hero";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  },[]);
 
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar />
+      <Hero />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
