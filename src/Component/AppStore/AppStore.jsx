4f84b906ddd86abfefd83079a94c377deccafa49
wacho91 <criskol.71@gmail.com>
2024-11-01 10:16:27 -0500

    AppStore Complete

--- src/Component/AppStore/AppStore.jsx
+++ src/Component/AppStore/AppStore.jsx
@@ -5,26 +5,26 @@ import Gif from "../../assets/mobile_bike.gif";
 
 const AppStore = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className="bg-gray-100 dark:bg-gray-800 py-14">
+        <div className="container">
+            <div className="grid sm:grid-cols-2 grid-cols-1 items-center gap-4">
                 <div>
-                    <h1>
+                    <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold text-gray-700 dark:text-gray-400">
                         Foodly is available for Android and IOS
                     </h1>
-                    <div>
+                    <div className="flex flex-wrap justify-center sm:justify-start items-center">
                         <a href="#">
                             <img 
                                 src={PlayStore} 
                                 alt="image" 
-
+                                className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
                             />
                         </a>
                         <a href="#">
                             <img 
                                 src={AppStoreImg} 
                                 alt="image" 
-                                
+                                className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
                             />
                         </a>
                     </div>
@@ -33,7 +33,7 @@ const AppStore = () => {
                     <img 
                         src={Gif} 
                         alt="image"
-                         
+                        className="w-full sm:max-w-[60%] block rounded-md mx-auto mix-blend-multiply dark:mix-blend-difference"
                     />
                 </div>
             </div>
