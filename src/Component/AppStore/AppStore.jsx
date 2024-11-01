6eb7410f91429f4d00875626c1751becac7e1cbd
wacho91 <criskol.71@gmail.com>
2024-11-01 10:17:56 -0500

    Animation Banner Complete

--- src/Component/AppStore/AppStore.jsx
+++ src/Component/AppStore/AppStore.jsx
@@ -8,7 +8,11 @@ const AppStore = () => {
     <div className="bg-gray-100 dark:bg-gray-800 py-14">
         <div className="container">
             <div className="grid sm:grid-cols-2 grid-cols-1 items-center gap-4">
-                <div>
+                <div
+                    data-aos="fade-up"
+                    data-aos-duration="300"
+                    className="space-y-6 max-w-xl mx-auto"
+                >
                     <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold text-gray-700 dark:text-gray-400">
                         Foodly is available for Android and IOS
                     </h1>
@@ -29,7 +33,7 @@ const AppStore = () => {
                         </a>
                     </div>
                 </div>
-                <div>
+                <div data-aos="zoom-in" data-aos-duration="300">
                     <img 
                         src={Gif} 
                         alt="image"
