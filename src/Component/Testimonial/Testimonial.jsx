3f458313a5021a809f6b67c1de1eab7c51ccece3
wacho91 <criskol.71@gmail.com>
2024-11-01 10:38:10 -0500

    Animation Testimonial Complete

--- src/Component/Testimonial/Testimonial.jsx
+++ src/Component/Testimonial/Testimonial.jsx
@@ -39,7 +39,7 @@ const Testimonial = () => {
     };
 
   return (
-    <div className="py-10">
+    <div data-aos="fade-up" data-aos-duration="300" className="py-10">
         <div className="container">
             <div className="text-center mb-20 max-w-[400px] mx-auto">
                 <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
@@ -51,7 +51,11 @@ const Testimonial = () => {
                     Perspiciatis delectus architecto error nesciunt,
                 </p>
             </div>
-            <div className="grid grid-cols-1 max-w-[600px] mx-auto gap-6">
+            <div
+                data-aos="zoom-in"
+                data-aos-duration="300" 
+                className="grid grid-cols-1 max-w-[600px] mx-auto gap-6"
+            >
                 <Slider {...settings}>
                     {testimonialData.map((data) => {
                         return (
